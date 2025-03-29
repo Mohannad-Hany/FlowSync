27dec9723b682d14f0896bc94675b2d1aeb972a4
Mohannad-Hany <92570900+Mohannad-Hany@users.noreply.github.com>
2025-03-29 17:55:04 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+let index = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+
+function showNextTestimonial() {
+    testimonials[index].classList.remove('active');
+    index = (index + 1) % testimonials.length;
+    testimonials[index].classList.add('active');
+}
+
+setInterval(showNextTestimonial, 2500);
